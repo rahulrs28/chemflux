ab4d68ba7d7506f9bdb83d3ee1779f5c0255ac28
Srivatsan Muralidharan <srivatsan.muralidharan@grainger.com>
2018-02-26 18:23:45 -0600

    Modified header section

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ $('#nav').affix({
 	var index=0;
 	$(document).scroll(function(){
 		var top = $('#skills').height()-$(window).scrollTop();
-		console.log(top)
+		// console.log(top)
 		if(top<-300){
 			if(index==0){	
 			
